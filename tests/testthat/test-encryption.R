d756448eeaebb1873006607ab6fd2aceaf11d370
Adelmo Filho <adelmo.aguiar.filho@gmail.com>
2019-03-05 16:33:22 -0300

    update testthat

--- tests/testthat/test-encryption.R
+++ tests/testthat/test-encryption.R
@@ -4,3 +4,7 @@ context("test-encryption")
 test_that("Message is from character class", {
   expect_that(encryption(2), throws_error())
 })
+
+test_that("TENIS-POLAR encryption", {
+  expect_true("tenis-polar" == encryption("polar-tenis", "polar", "tenis"))
+})
